907ce6e4a02b64ce6c2f79ec9130b0e7556e0f56
ColeAnderson7278 <canderson@basecampcodingacademy.org>
2018-07-09 15:40:28 -0500

    finished core and test_core

--- core.py
+++ core.py
@@ -1,2 +1,58 @@
+from random import randint
+
+
 def new_gladiator(health, rage, damage_low, damage_high):
-    return {'Health': health,'Rage': rage,'D'}
+    return {
+        'Health': health,
+        'Rage': rage,
+        'Damage Low': damage_low,
+        'Damage High': damage_high
+    }
+
+
+def attack(attacker, defender):
+    if attacker['Rage'] == 0:
+        for randint in range(attacker['Damage Low'], attacker['Damage High']):
+            attack = randint
+            defender['Health'] = defender['Health'] - attack
+            attacker['Rage'] = attacker['Rage'] + 15
+            return defender['Health']
+
+    if attacker['Rage'] > 0:
+        for randint in range(1, 100):
+            if randint <= attacker['Rage']:
+                for randint in range(attacker['Damage Low'],
+                                     attacker['Damage High']):
+                    attack = randint * 2
+                    defender['Health'] = defender['Health'] - attack
+                    attacker['Rage'] = 0
+                    return defender['Health']
+
+        else:
+            for randint in range(attacker['Damage Low'],
+                                 attacker['Damage High']):
+                attack = randint
+                defender['Health'] = defender['Health'] - attack
+                attacker['Rage'] = attacker['Rage'] + 15
+                return defender['Health']
+
+
+def heal(gladiator):
+    if gladiator['Rage'] >= 10:
+        if gladiator['Health'] == 100:
+            return None
+        if gladiator['Health'] in range(96, 99):
+            gladiator['Rage'] = gladiator['Rage'] - 10
+            gladiator['Health'] = 100
+            return gladiator
+        if gladiator['Health'] <= 95:
+            gladiator['Rage'] = gladiator['Rage'] - 10
+            gladiator['Health'] = gladiator['Health'] + 5
+            return gladiator
+    else:
+        return None
+
+
+def is_dead(gladiator):
+    if gladiator['Health'] <= 0:
+        return True
---
+++ test_core.py
@@ -0,0 +1,78 @@
+from core import *
+
+
+def test_new_gladiator():
+    assert new_gladiator(55, 35, 5, 25) == ({
+        'Health': 55,
+        'Rage': 35,
+        'Damage Low': 5,
+        'Damage High': 25
+    })
+    assert new_gladiator(100, 0, 5, 25) == ({
+        'Health': 100,
+        'Rage': 0,
+        'Damage Low': 5,
+        'Damage High': 25
+    })
+    assert new_gladiator(0, 0, 0, 0) == ({
+        'Health': 0,
+        'Rage': 0,
+        'Damage Low': 0,
+        'Damage High': 0
+    })
+
+
+def test_attack():
+    attacker = {'Health': 100, 'Rage': 0, 'Damage Low': 5, 'Damage High': 25}
+    defender = {'Health': 100, 'Rage': 0, 'Damage Low': 5, 'Damage High': 25}
+    assert attack(attacker, defender) == defender['Health']
+
+    attacker = {'Health': 55, 'Rage': 50, 'Damage Low': 5, 'Damage High': 25}
+    defender = {'Health': 70, 'Rage': 10, 'Damage Low': 5, 'Damage High': 25}
+    assert attack(attacker, defender) == defender['Health']
+
+
+def test_heal():
+    assert heal({
+        'Health': 55,
+        'Rage': 10,
+        'Damage Low': 5,
+        'Damage High': 25
+    }) == ({
+        'Health': 60,
+        'Rage': 0,
+        'Damage Low': 5,
+        'Damage High': 25
+    })
+    assert heal({
+        'Health': 100,
+        'Rage': 0,
+        'Damage Low': 5,
+        'Damage High': 25
+    }) == None
+    assert heal({
+        'Health': 97,
+        'Rage': 20,
+        'Damage Low': 5,
+        'Damage High': 25
+    }) == ({
+        'Health': 100,
+        'Rage': 10,
+        'Damage Low': 5,
+        'Damage High': 25
+    })
+
+
+def test_is_dead():
+    assert is_dead({
+        'Health': 0,
+        'Rage': 0,
+        'Damage Low': 5,
+        'Damage High': 25
+    }) == True
+    assert is_dead({
+        'Health': 100,
+        'Rage': 0,
+        'Damage Low': 5,
+        'Damage High': 25
+    }) == None
